9ecb373d9850bb0f19003d90b06f1afa2164a17b
Matias <matias.iturrieta.hdz@gmail.com>
2021-08-28 19:04:40 -0400

    gato/reversi

--- reversi.py
+++ reversi.py
@@ -1,16 +1,13 @@
-import cocos
-class Laberinto:
+import cocos2d
+class Tablero:
     def __init__(self):
-        self.sprite=cocos.sprite.Sprite("resources/reversi.png")
+        self.sprite=cocos2d.sprite.Sprite("resources/reversi.png")
         self.sprite.position=270,270
         self.mapa= [
             [0,0,0,0,0,0],
-            [0,1,0,1,1,1],
             [0,0,0,0,0,0],
-            [1,1,0,1,0,1],
-            [0,1,0,1,1,1,0,1,0],
-            [0,1,0,0,0,0,0,1,0],
-            [0,0,0,1,1,1,0,0,0],
-            [0,1,0,1,0,1,0,1,0],
-            [0,0,0,0,0,0,0,0,0]
+            [0,0,0,0,0,0],
+            [0,0,0,0,0,0],
+            [0,0,0,0,0,0],
+            [0,0,0,0,0,0]
         ]
