32e43410b8f9d0efbb049c619974f06ce5d4a3cc
Matias <matias.iturrieta.hdz@gmail.com>
2021-09-12 19:14:33 -0300

    latest

--- aisearch.py
+++ aisearch.py
@@ -7,162 +7,183 @@ diagonalesIzquierda = test.diagonalesIzquierda
 tamanio = 6*6
 class JuegoGato:
   #Comienza el raton, valor=1
-  def __init__(self,estado=[0]*tamanio,turno=1):
-    self.tablero=estado
+  def __init__(self,turno=1):
+    self.tablero=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,-1,0,0,0,0,0,-1,1,0,0,0,0,0,0,0,0,0,0,0,0,0]
     self.completo=False
     self.ganador=None
     self.jugador=turno
+    self.fichasPorDarVuelta=[]
 
   def reiniciar(self):
-    self.tablero=[0]*tamanio
+    self.tablero=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,-1,0,0,0,0,0,-1,1,0,0,0,0,0,0,0,0,0,0,0,0,0]
     self.completo=False
     self.ganador=None
     self.jugador=1
+    self.fichasPorDarVuelta = []
+
+  def voltearFichas(self):
+    for posFicha in self.fichasPorDarVuelta:
+      self.tablero[posFicha] = self.tablero[posFicha] * -1
+    self.fichasPorDarVuelta = []
 
   def revisarDiagonalSuperiorDer(self, pos):
-      for diagonal in diagonalesDerecha:
-          if pos in diagonal:
-              arrDiagonal = diagonal
-      cotaInferior = arrDiagonal[0]
-      posicionDeFichasPorDarVuelta = []
-      if(pos - 10 >= cotaInferior and self.tablero[pos - 5] == self.jugador * -1):
-          indicePosEnColumnas = arrDiagonal.index(pos)
-          arrayCasillasPorVerificar = arrDiagonal[0:indicePosEnColumnas]
-          for posActual in arrayCasillasPorVerificar.__reversed__():
-              fichaActual = self.tablero[posActual]
-              if(fichaActual != self.jugador):
-                  posicionDeFichasPorDarVuelta.append(posActual)
-              if(fichaActual == self.jugador):
-                  print('Fichas a dar vuelta: ', posicionDeFichasPorDarVuelta)
-                  return True
-      return False
+    arrDiagonal = []
+    cotaInferior = None
+    for diagonal in diagonalesDerecha:
+      if pos in diagonal:
+        arrDiagonal = diagonal
+        cotaInferior = arrDiagonal[0]
+    posicionDeFichasPorDarVuelta = []
+    if(cotaInferior != None and pos - 10 >= cotaInferior and self.tablero[pos - 5] == self.jugador * -1):
+      indicePosEnColumnas = arrDiagonal.index(pos)
+      arrayCasillasPorVerificar = arrDiagonal[0:indicePosEnColumnas]
+      for posActual in arrayCasillasPorVerificar.__reversed__():
+        fichaActual = self.tablero[posActual]
+        if(fichaActual != self.jugador):
+          posicionDeFichasPorDarVuelta.append(posActual)
+          self.fichasPorDarVuelta = posicionDeFichasPorDarVuelta
+        if(fichaActual == self.jugador):
+          print('Fichas a dar vuelta: ', self.fichasPorDarVuelta)
+          return True
+    self.voltearFichas()
+    return False
   def revisarDiagonalSuperiorIzq(self, pos):
-      for diagonal in diagonalesIzquierda:
-          if pos in diagonal:
-              arrDiagonal = diagonal
-      cotaInferior = arrDiagonal[0]
-      posicionDeFichasPorDarVuelta = []
-      if(pos - 14 >= cotaInferior and self.tablero[pos - 7] == self.jugador * -1): 
-          indicePosEnColumnas = arrDiagonal.index(pos)
-          arrayCasillasPorVerificar = arrDiagonal[0:indicePosEnColumnas]
-          for posActual in arrayCasillasPorVerificar.__reversed__():
-              fichaActual = self.tablero[posActual]
-              if(fichaActual != self.jugador):
-                  posicionDeFichasPorDarVuelta.append(posActual)
-              if(fichaActual == self.jugador):
-                  print('Fichas a dar vuelta: ', posicionDeFichasPorDarVuelta)
-                  return True
-      return False 
+    arrDiagonal = []
+    cotaInferior = None
+    for diagonal in diagonalesIzquierda:
+      if pos in diagonal:
+        arrDiagonal = diagonal
+        cotaInferior = arrDiagonal[0]
+    posicionDeFichasPorDarVuelta = []
+    if(cotaInferior != None and pos - 14 >= cotaInferior and self.tablero[pos - 7] == self.jugador * -1): 
+      indicePosEnColumnas = arrDiagonal.index(pos)
+      arrayCasillasPorVerificar = arrDiagonal[0:indicePosEnColumnas]
+      for posActual in arrayCasillasPorVerificar.__reversed__():
+        fichaActual = self.tablero[posActual]
+        if(fichaActual != self.jugador):
+          posicionDeFichasPorDarVuelta.append(posActual)
+        if(fichaActual == self.jugador):
+          print('Fichas a dar vuelta: ', posicionDeFichasPorDarVuelta)
+          return True
+    return False 
   def revisarHaciaAbajo(self, pos):
     numColumna = 0
+    cotaSuperior = None
     for fila in filas:
-        if pos in fila:
-            numColumna = fila.index(pos)
-    cotaSuperior = columnas[numColumna][-1]
+      if pos in fila:
+        numColumna = fila.index(pos)
+        cotaSuperior = columnas[numColumna][-1]
     posicionDeFichasPorDarVuelta = []
     #Deben haber al menos 2 espacios hacia abajo, y la ficha de la casilla de abajo debe ser del contrincante
-    if(pos + 12 <= cotaSuperior and self.tablero[pos + 6] == self.jugador * -1): 
-        indicePosEnColumnas = columnas[numColumna].index(pos)
-        arrayCasillasPorVerificar = columnas[numColumna][indicePosEnColumnas+1:]
-        for posActual in arrayCasillasPorVerificar:
-            fichaActual = self.tablero[posActual]
-            if(fichaActual != self.jugador):
-                posicionDeFichasPorDarVuelta.append(posActual)
-            if(fichaActual == self.jugador):
-                print('Fichas a dar vuelta: ', posicionDeFichasPorDarVuelta)
-                return True
+    if(cotaSuperior != None and pos + 12 <= cotaSuperior and self.tablero[pos + 6] == self.jugador * -1): 
+      indicePosEnColumnas = columnas[numColumna].index(pos)
+      arrayCasillasPorVerificar = columnas[numColumna][indicePosEnColumnas+1:]
+      for posActual in arrayCasillasPorVerificar:
+        fichaActual = self.tablero[posActual]
+        if(fichaActual != self.jugador):
+          posicionDeFichasPorDarVuelta.append(posActual)
+        if(fichaActual == self.jugador):
+          print('Fichas a dar vuelta: ', posicionDeFichasPorDarVuelta)
+          return True
     return False
   def revisarHaciaArriba(self, pos):
     numColumna = 0
+    cotaInferior = None
     for fila in filas:
-        if pos in fila:
-            numColumna = fila.index(pos)
-    cotaInferior = columnas[numColumna][0]
+      if pos in fila:
+        numColumna = fila.index(pos)
+        cotaInferior = columnas[numColumna][0]
     posicionDeFichasPorDarVuelta = []
     #Deben haber al menos 2 espacios hacia arriba, y la ficha de la casilla de arriba debe ser del contrincante
-    if(pos - 12 >= cotaInferior and self.tablero[pos - 6] == self.jugador * -1): 
-        indicePosEnColumnas = columnas[numColumna].index(pos)
-        arrayCasillasPorVerificar = columnas[numColumna][0:indicePosEnColumnas]
-        for posActual in arrayCasillasPorVerificar.__reversed__():
-            fichaActual = self.tablero[posActual]
-            if(fichaActual != self.jugador):
-                posicionDeFichasPorDarVuelta.append(posActual)
-            if(fichaActual == self.jugador):
-                print('Fichas a dar vuelta: ', posicionDeFichasPorDarVuelta)
-                return True
+    if(cotaInferior != None and pos - 12 >= cotaInferior and self.tablero[pos - 6] == self.jugador * -1): 
+      indicePosEnColumnas = columnas[numColumna].index(pos)
+      arrayCasillasPorVerificar = columnas[numColumna][0:indicePosEnColumnas]
+      for posActual in arrayCasillasPorVerificar.__reversed__():
+        fichaActual = self.tablero[posActual]
+        if(fichaActual != self.jugador):
+          posicionDeFichasPorDarVuelta.append(posActual)
+        if(fichaActual == self.jugador):
+          print('Fichas a dar vuelta: ', posicionDeFichasPorDarVuelta)
+          return True
     return False
   def revisarHaciaIzquierda(self, pos):
     numFila = 0
+    cotaInferior = None
     for fila in filas:
-        if pos in fila:
-            numFila = filas.index(fila)
-    cotaInferior = filas[numFila][0]
+      if pos in fila:
+        numFila = filas.index(fila)
+        cotaInferior = filas[numFila][0]
     posicionDeFichasPorDarVuelta = []
     #Deben haber al menos 2 espacios hacia la izquierda, y la ficha de la casilla izquierda debe ser del contrincante
-    if(pos - 2 >= cotaInferior and self.tablero[pos - 1] == self.jugador * -1): 
-        indicePosEnColumnas = filas[numFila].index(pos)
-        arrayCasillasPorVerificar = filas[numFila][0:indicePosEnColumnas]
-        for posActual in arrayCasillasPorVerificar.__reversed__():
-            fichaActual = self.tablero[posActual]
-            if(fichaActual != self.jugador):
-              posicionDeFichasPorDarVuelta.append(posActual)
-            if(fichaActual == self.jugador):
-                print('Fichas a dar vuelta: ', posicionDeFichasPorDarVuelta)
-                return True
+    if(cotaInferior != None and pos - 2 >= cotaInferior and self.tablero[pos - 1] == self.jugador * -1): 
+      indicePosEnColumnas = filas[numFila].index(pos)
+      arrayCasillasPorVerificar = filas[numFila][0:indicePosEnColumnas]
+      for posActual in arrayCasillasPorVerificar.__reversed__():
+        fichaActual = self.tablero[posActual]
+        if(fichaActual != self.jugador):
+          posicionDeFichasPorDarVuelta.append(posActual)
+        if(fichaActual == self.jugador):
+          print('Fichas a dar vuelta: ', posicionDeFichasPorDarVuelta)
+          return True
     return False
   def revisarHaciaDerecha(self, pos):
     numFila = 0
+    cotaSuperior = None
     for fila in filas:
-        if pos in fila:
-            numFila = filas.index(fila)
-    cotaSuperior = filas[numFila][-1]
+      if pos in fila:
+        numFila = filas.index(fila)
+        cotaSuperior = filas[numFila][-1]
     posicionDeFichasPorDarVuelta = []
     #Deben haber al menos 2 espacios hacia la derecha, y la ficha de la casilla derecha debe ser del contrincante
-    if(pos + 2 <= cotaSuperior and self.tablero[pos + 1] == self.jugador * -1): 
-        indicePosEnColumnas = filas[numFila].index(pos)
-        arrayCasillasPorVerificar = filas[numFila][indicePosEnColumnas+1:]
-        for posActual in arrayCasillasPorVerificar:
-            fichaActual = self.tablero[posActual]
-            if(fichaActual != self.jugador):
-              posicionDeFichasPorDarVuelta.append(posActual)
-            if(fichaActual == self.jugador):
-                print('Fichas a dar vuelta: ', posicionDeFichasPorDarVuelta)
-                return True
+    if(cotaSuperior != None and pos + 2 <= cotaSuperior and self.tablero[pos + 1] == self.jugador * -1): 
+      indicePosEnColumnas = filas[numFila].index(pos)
+      arrayCasillasPorVerificar = filas[numFila][indicePosEnColumnas+1:]
+      for posActual in arrayCasillasPorVerificar:
+        fichaActual = self.tablero[posActual]
+        if(fichaActual != self.jugador):
+          posicionDeFichasPorDarVuelta.append(posActual)
+        if(fichaActual == self.jugador):
+          print('Fichas a dar vuelta: ', posicionDeFichasPorDarVuelta)
+          return True
     return False
   def revisarDiagonalInferiorIzq(self, pos):
+    arrDiagonal = []
+    cotaSuperior = None
     for diagonal in diagonalesDerecha:
-        if pos in diagonal:
-            arrDiagonal = diagonal
-    cotaSuperior = arrDiagonal[-1]
+      if pos in diagonal:
+        arrDiagonal = diagonal
+        cotaSuperior = arrDiagonal[-1]
     posicionDeFichasPorDarVuelta = []
-    if(pos + 10 <= cotaSuperior and self.tablero[pos + 5] == self.jugador * -1):
-        indicePosEnColumnas = arrDiagonal.index(pos)
-        arrayCasillasPorVerificar = arrDiagonal[indicePosEnColumnas+1:]
-        for posActual in arrayCasillasPorVerificar:
-            fichaActual = self.tablero[posActual]
-            if(fichaActual != self.jugador):
-                posicionDeFichasPorDarVuelta.append(posActual)
-            if(fichaActual == self.jugador):
-                print('Fichas a dar vuelta: ', posicionDeFichasPorDarVuelta)
-                return True
+    if(cotaSuperior != None and pos + 10 <= cotaSuperior and self.tablero[pos + 5] == self.jugador * -1):
+      indicePosEnColumnas = arrDiagonal.index(pos)
+      arrayCasillasPorVerificar = arrDiagonal[indicePosEnColumnas+1:]
+      for posActual in arrayCasillasPorVerificar:
+        fichaActual = self.tablero[posActual]
+        if(fichaActual != self.jugador):
+          posicionDeFichasPorDarVuelta.append(posActual)
+        if(fichaActual == self.jugador):
+          print('Fichas a dar vuelta: ', posicionDeFichasPorDarVuelta)
+          return True
     return False
   def revisarDiagonalInferiorDer(self, pos):
-      for diagonal in diagonalesIzquierda:
-          if pos in diagonal:
-              arrDiagonal = diagonal
-      cotaSuperior = arrDiagonal[-1]
-      posicionDeFichasPorDarVuelta = []
-      if(pos + 14 <= cotaSuperior and self.tablero[pos + 7] == self.jugador * -1): 
-          indicePosEnColumnas = arrDiagonal.index(pos)
-          arrayCasillasPorVerificar = arrDiagonal[indicePosEnColumnas+1:]
-          for posActual in arrayCasillasPorVerificar:
-              fichaActual = self.tablero[posActual]
-              if(fichaActual != self.jugador):
-                  posicionDeFichasPorDarVuelta.append(posActual)
-              if(fichaActual == self.jugador):
-                  print('Fichas a dar vuelta: ', posicionDeFichasPorDarVuelta)
-                  return True
-      return False
+    arrDiagonal = []
+    cotaSuperior = None
+    for diagonal in diagonalesIzquierda:
+      if pos in diagonal:
+        arrDiagonal = diagonal
+        cotaSuperior = arrDiagonal[-1]
+    posicionDeFichasPorDarVuelta = []
+    if(cotaSuperior != None and pos + 14 <= cotaSuperior and self.tablero[pos + 7] == self.jugador * -1): 
+      indicePosEnColumnas = arrDiagonal.index(pos)
+      arrayCasillasPorVerificar = arrDiagonal[indicePosEnColumnas+1:]
+      for posActual in arrayCasillasPorVerificar:
+        fichaActual = self.tablero[posActual]
+        if(fichaActual != self.jugador):
+          posicionDeFichasPorDarVuelta.append(posActual)
+        if(fichaActual == self.jugador):
+          print('Fichas a dar vuelta: ', posicionDeFichasPorDarVuelta)
+          return True
+    return False
   
   def generar_jugadas_posibles(self): #Acá se ve que, porque y como jugara el agente
     posibles=[]
@@ -232,7 +253,7 @@ class JuegoGato:
     self.jugador*=-1
 
 #IA
-def alfabeta(juego,etapa,alfa,beta,secuencia,secuencias):
+def alfabeta(juego,etapa, alfa,beta,secuencia,secuencias):
   if juego.estado_final():
     secuencias.append(secuencia.copy())
     return [-1*juego.calcular_utilidad()]
@@ -244,7 +265,7 @@ def alfabeta(juego,etapa,alfa,beta,secuencia,secuencias):
   for jugada in jugadas_posibles:
     juego.jugar(jugada)
     secuencia.append(jugada)
-    opcion=alfabeta(juego,etapa*-1,alfa,beta,secuencia,secuencias)
+    opcion=alfabeta(juego, etapa*-1, alfa, beta, secuencia, secuencias)
     if etapa==1:
       if valor[0]<opcion[0]:
         valor=[opcion[0],jugada]
@@ -269,6 +290,6 @@ def alfabeta(juego,etapa,alfa,beta,secuencia,secuencias):
 
 
 if __name__ == "__main__":
-  juego=JuegoGato([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],-1)
+  juego=JuegoGato(-1)
   o3=[]
-  r3=alfabeta(juego,-1,-1000,1000,[],o3)
+  r3=alfabeta(juego,-1, 5,-1000,1000,[],o3)
